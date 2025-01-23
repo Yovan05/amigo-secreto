428abed696866c9ac8f4213415ec0542d15f827c
Yovan Rodriguez <yovanr129@gmail.com>
2025-01-23 08:57:23 -0700

    Se agrego la funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(amigo){
+    let regex = /^[A-Za-z\s]+$/;
+
+    if(amigo === ""){
+        alert("El campo no puede estar vacío");
+        return;
+    }
+
+    if(!regex.test(amigo)){
+        alert("Solo se permiten letras");
+        return;
+    }
+
+    amigos.push(amigo);
+    return true;
+}
+
+function mostrarAmigos(amigos){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = '';
+    for(let amigo of amigos){
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+    return;
+}
+
+function limpiarCaja(){
+    document.getElementById("amigo").value = '';
+    return;
+}
+
+function botonAgregar(){
+    let amigo = document.getElementById("amigo").value;
+    agregarAmigo(amigo);
+    mostrarAmigos(amigos);
+    limpiarCaja();
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
